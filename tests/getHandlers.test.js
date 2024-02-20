cc252452430c5236257b5eb777e5c834022debf6
Natasha Bagramian <natalia.bagramian@gmail.com>
2024-02-19 17:34:13 -0800

    project7

--- tests/getHandlers.test.js
+++ tests/getHandlers.test.js
@@ -23,19 +23,3 @@ test("there are 4 delivery services", async () => {
   const countServices = data.length;
   expect(countServices).toBe(4);
 });
-
-// TEST 3 RESPONSE BODY - WORKING HOURS
-test("working hours shown for all deliveries", async () => {
-  let responseBodyHours;
-  try {
-    responseBodyHours = await fetch(`${config.API_URL}/api/v1/couriers`);
-  } catch (error) {
-    console.error(error);
-  }
-  const dataDelivery = responseBodyHours.json();
-  const result = [];
-  for (let i = 0; i < dataDelivery.length; i++) {
-    if ("workingHours") result.push(true);
-  }
-  expect(result).not.toContain(false);
-});
